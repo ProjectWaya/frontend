75c574ebf230d029796a853b59ba39e3d853e4d9
Robert Schenkenfelder <robert.schenkenfelder@runtastic.com>
2015-11-26 11:13:00 +0100

    ADD labels ADD constants

--- app/controllers/onboarding.js
+++ app/controllers/onboarding.js
@@ -1,16 +1,23 @@
 import Ember from 'ember';
 
+const countries = ['France', 'Austria', 'Germany', 'Spain'];
+const cities = ['Paris', 'Lyon', 'Marseille', 'Bordeaux'];
+
 export default Ember.Controller.extend({
-  countries: ['France', 'Austria', 'Germany', 'Spain'],
-  cities: ['Paris', 'Lyon', 'Marseille', 'Bordeaux'],
+  countries: countries,
+  defaultCountry: countries[0],
+  cities: cities,
+  defaultCity: cities[0],
   roles: ['refugee', 'asyliumSeeker', 'justArrived'],
   currentRole: 'refugee',
   actions: {
     onCountrySelected() {
-      console.log("selected country");
+      console.log("selected country: " + country);
+      this.set('defaultCountry', country);
     },
     onCitySelected() {
-      console.log("selected city");
+      console.log("selected city: " + city);
+      this.set('defaultCity', city);
     }
   }
 });
