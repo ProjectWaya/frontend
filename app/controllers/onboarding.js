63005b631aff9968a6e081b80c002e9b2175bcc1
Mathias Aichinger <mathias.aichinger@runtastic.com>
2015-11-26 10:54:44 +0100

    ADD first draft of role chooser

--- app/controllers/onboarding.js
+++ app/controllers/onboarding.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	countries: ['France', 'Austria', 'Germany', 'Spain'],
-	cities: ['Paris', 'Lyon', 'Marseille', 'Bordeaux'],
-	actions: {
-		onCountrySelected() { 
-			console.log("selected country");
-    	},
-    	onCitySelected() {
-    		console.log("selected city");
-    	}
-	}
+  countries: ['France', 'Austria', 'Germany', 'Spain'],
+  cities: ['Paris', 'Lyon', 'Marseille', 'Bordeaux'],
+  roles: ['refugee', 'asyliumSeeker', 'justArrived'],
+  currentRole: 'refugee',
+  actions: {
+    onCountrySelected() {
+      console.log("selected country");
+    },
+    onCitySelected() {
+      console.log("selected city");
+    },
+    roleChanged() {
+
+    }
+  }
 });
