064429cbadd46efd13896aef8ef84f9869a51418
Hector Leon Zarco Garcia <hezarco@gmail.com>
2015-11-26 09:34:32 +0100

    Route boilerplate

--- app/router.js
+++ app/router.js
@@ -6,6 +6,18 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('home');
+  this.route('onboarding');
+  this.route('finder', function() {
+    this.route('shelter');
+    this.route('health');
+    this.route('transportation');
+    this.route('asylum');
+    this.route('education');
+    this.route('administrative');
+    this.route('entertainment');
+    this.route('food');
+  });
 });
 
 export default Router;
