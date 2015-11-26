97ec16fad21ec99bafb0d287af49f7b20c0121e2
Mathias Aichinger <mathias.aichinger@runtastic.com>
2015-11-26 14:47:44 +0100

    MOD use the new user-status objects which are coming from the backend

--- app/routes/onboarding.js
+++ app/routes/onboarding.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+
+  //TODO: extend model for cities and countries
+  model() {
+    return this.get('store').find('user-status');
+  }
 });
