62e8b6213bc24c448304ec29de5c610d78254278
Robert Schenkenfelder <robert.schenkenfelder@runtastic.com>
2015-11-26 14:50:33 +0100

    ADD german footer localization MOD localizations default to english REM Italian (not going to be translated in first place)

--- app/locales/de/translations.js
+++ app/locales/de/translations.js
@@ -1,9 +1,40 @@
 export default {
-	"en": "Englisch",
-	"fr": "Französisch",
-	"es": "Spanisch",
-	"de": "Deutsch",
-	"tr": "Türkisch",
-	"ru": "Russisch",
-	"zh": "Chinesisch"
+  "en": "Englisch",
+  "fr": "Französisch",
+  "es": "Spanisch",
+  "de": "Deutsch",
+  "tr": "Türkisch",
+  "ru": "Russisch",
+  "zh": "Chinesisch",
+  "pages": {
+    "index": {
+      "headline": "Willkommen",
+      "tagline": "Informationen und Hilfe für Flüchtlinge und Aslywerber",
+      "buttons": {
+        "start": "Starte"
+      },
+      "footer": {
+        "sections": {
+          "navigation": {
+            "about": "Über WAYA",
+            "team": "Team",
+            "participate": "Mitmachen",
+            "values": "Werte",
+            "contact": "Kontakt",
+            "projects": "Unsere Projekte"
+          },
+          "social": {
+            "headline": "Folge uns"
+          },
+          "partners": {
+            "headline": "Partner"
+          },
+          "bottom": {
+            "admin": "Admin Login",
+            "terms": "Nutzungsbestimmungen"
+          }
+        }
+      }
+    }
+  }
 }
