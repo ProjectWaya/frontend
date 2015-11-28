4d8b358518d49d13a1484123ebfd0f2b2317226e
Hector Leon Zarco Garcia <hezarco@gmail.com>
2015-11-28 20:38:17 +0100

    Fix points property

--- app/controllers/home/overview/category.js
+++ app/controllers/home/overview/category.js
@@ -26,9 +26,9 @@ export default Ember.Controller.extend({
     }.bind(this));
   }),
 
-  points: Ember.computed({
+  points: Ember.computed('model.points', {
     get() {
-      return this.get('model').map((p) => {
+      return this.get('model.points').map((p) => {
         return {
           id: p.get('id'),
           location: L.latLng(p.get('latitude'), p.get('longitude')),
