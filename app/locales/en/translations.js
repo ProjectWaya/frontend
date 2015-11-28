8ddc3848177a41fd05ddab9ecab7d5d6b0ea13a1
Robert Schenkenfelder <robert.schenkenfelder@runtastic.com>
2015-11-28 15:29:25 +0100

    ADD page navigation breadcrumb for footer page links

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -38,6 +38,9 @@ export default {
           }
         }
       },
+      "header": {
+        "home": "Home"
+      },
       "about_waya": {
         "name": "About Waya",
         "content": `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
