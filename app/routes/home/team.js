3f36b71b95bf85e775ca96ea6ce7140d0fdf3b80
Robert Schenkenfelder <robert.schenkenfelder@runtastic.com>
2015-11-28 14:15:41 +0100

    MOD scroll to top smoothly when footer link is clicked

--- app/routes/home/team.js
+++ app/routes/home/team.js
@@ -1,3 +1,8 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+  activate: function() {
+    this._super();
+    Ember.$("body").animate({ scrollTop: 0 }, '300', 'easeOutQuad');
+  }
+});
